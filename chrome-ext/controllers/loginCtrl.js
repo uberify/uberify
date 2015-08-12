29caa6a74cef25acac278a4c51edf869413474f1
Nick Balestra <nick@balestra.ch>
2015-08-11 21:49:50 -0700

    testing triggering an icon change

--- chrome-ext/controllers/loginCtrl.js
+++ chrome-ext/controllers/loginCtrl.js
@@ -1,6 +1,8 @@
 angular.module('app.login', [])
   .controller('loginController', function($scope, nav){
     $scope.login = function(){
+      // Testing triggering an icon change
+      chrome.browserAction.setIcon({path: 'iconActive.png'});
       nav.addresses();
     }
   })
